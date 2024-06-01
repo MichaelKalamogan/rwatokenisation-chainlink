64527f0256b4db37561945efe3d71f3dc14ac9e5
Michael Kalamogan <michael@cakedefi.com>
2024-06-01 13:41:04 +0800

    feat: file upload feature

--- frontend/src/components/FileInput/FileInput.jsx
+++ frontend/src/components/FileInput/FileInput.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './FileInput.css';
+import ClipLoader from 'react-spinners/ClipLoader';
+import './FileInput.css'
 
 function FileInput(props) {
+  const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
   const [assetType, setAssetType] = useState('');
   const [assetName, setAssetName] = useState('');
@@ -22,60 +24,73 @@ function FileInput(props) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (file) {
+      setLoading(true);
       try {
-        // const formData = new FormData();
-        // formData.append('file', file);
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('assetType', assetType);
+        formData.append('assetName', assetName);
 
         const response = await axios.post(
-          '/https://assetvalidator-simulator.onrender.com/api/validate',
-          {
-            method: 'POST',
-            body: {
-              "assetType": assetType,
-              "assetName": assetName
-            }
-          },
+          'https://assetvalidator-simulator.onrender.com/api/validate',
+          formData,
         );
 
-        if (response.ok) {
+        if (response.status === 200) {
           console.log('File successfully uploaded');
         } else {
           console.error('File upload failed');
         }
       } catch (error) {
         console.error('An error occurred during file upload:', error);
-      }
+      } 
+
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      setLoading(false);
+      
     } else {
-      console.log('No file selected');
+      window.alert('No file selected');
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='assetName'>Asset Name:</label>
-        <input
-          type='text'
-          id='assetName'
-          value={assetName}
-          onChange={handleAssetNameChange}
-        />
-        <br />
-        <label htmlFor='assetType'>Select Asset Type:</label>
-        <select
-          id='assetType'
-          value={assetType}
-          onChange={handleAssetTypeChange}
-        >
-          <option value=''>--Please choose an option--</option>
-          <option value='Real Estate'>Real Estate</option>
-          <option value='Bond'>Bond</option>
-          <option value='Funds'>Funds</option>
-        </select>
-        <br />
-        <input type='file' onChange={handleFileChange} />
-        <br />
-        <button onClick={handleSubmit}>Submit</button>
+<div className="file-input-container">
+      <ClipLoader
+        color='blue'
+        loading={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      <form onSubmit={handleSubmit} className="file-input-form">
+        <div className="form-group">
+          <label htmlFor='assetName'>Asset Name:</label>
+          <input
+            type='text'
+            id='assetName'
+            value={assetName}
+            onChange={handleAssetNameChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor='assetType'>Select Asset Type:</label>
+          <select
+            id='assetType'
+            value={assetType}
+            onChange={handleAssetTypeChange}
+            className="form-control"
+          >
+            <option value=''>--Please choose an option--</option>
+            <option value='Real Estate'>Real Estate</option>
+            <option value='Bond'>Bond</option>
+            <option value='Funds'>Funds</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <input type='file' onChange={handleFileChange} className="form-control" />
+        </div>
+        <button type='submit' className="submit-button">Submit</button>
       </form>
     </div>
   );
