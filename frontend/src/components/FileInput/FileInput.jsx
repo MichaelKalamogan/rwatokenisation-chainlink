0d49c3e2464411e5db2c4b1c9546fe8f0432d631
Michael Kalamogan <michael@cakedefi.com>
2024-06-01 13:47:24 +0800

    fix: css

--- frontend/src/components/FileInput/FileInput.jsx
+++ frontend/src/components/FileInput/FileInput.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import ClipLoader from 'react-spinners/ClipLoader';
-import './FileInput.css'
+import './FileInput.css';
 
 function FileInput(props) {
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
   const [assetType, setAssetType] = useState('');
   const [assetName, setAssetName] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleAssetTypeChange = (e) => {
     setAssetType(e.target.value);
@@ -36,63 +37,75 @@ function FileInput(props) {
           formData,
         );
 
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+        setLoading(false);
+
         if (response.status === 200) {
-          console.log('File successfully uploaded');
+          window.alert('File successfully verified');
         } else {
-          console.error('File upload failed');
+          setMessage('File upload failed');
         }
       } catch (error) {
-        console.error('An error occurred during file upload:', error);
-      } 
-
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      setLoading(false);
-      
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+        setLoading(false);
+        setMessage('An error occurred during file upload');
+        console.log(error);
+      }
     } else {
       window.alert('No file selected');
     }
   };
 
   return (
-<div className="file-input-container">
-      <ClipLoader
-        color='blue'
-        loading={loading}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-      <form onSubmit={handleSubmit} className="file-input-form">
-        <div className="form-group">
-          <label htmlFor='assetName'>Asset Name:</label>
-          <input
-            type='text'
-            id='assetName'
-            value={assetName}
-            onChange={handleAssetNameChange}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor='assetType'>Select Asset Type:</label>
-          <select
-            id='assetType'
-            value={assetType}
-            onChange={handleAssetTypeChange}
-            className="form-control"
-          >
-            <option value=''>--Please choose an option--</option>
-            <option value='Real Estate'>Real Estate</option>
-            <option value='Bond'>Bond</option>
-            <option value='Funds'>Funds</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <input type='file' onChange={handleFileChange} className="form-control" />
-        </div>
-        <button type='submit' className="submit-button">Submit</button>
-      </form>
-    </div>
+    <div className="file-input-container">
+    <ClipLoader
+      color='blue'
+      loading={loading}
+      size={150}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
+    <form onSubmit={handleSubmit} className="file-input-form">
+      <div className="form-group">
+        <label htmlFor='assetName'>Asset Name:</label>
+        <input
+          type='text'
+          id='assetName'
+          value={assetName}
+          onChange={handleAssetNameChange}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor='assetType'>Select Asset Type:</label>
+        <select
+          id='assetType'
+          value={assetType}
+          onChange={handleAssetTypeChange}
+          className="form-control"
+        >
+          <option value=''>--Please choose an option--</option>
+          <option value='Real Estate'>Real Estate</option>
+          <option value='Bond'>Bond</option>
+          <option value='Funds'>Funds</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <input
+          type='file'
+          onChange={handleFileChange}
+          className="form-control"
+        />
+      </div>
+      <button type='submit' className="submit-button">Submit</button>
+    </form>
+
+    {message && (
+      <div className="message-container">
+        <h3>{message}</h3>
+      </div>
+    )}
+  </div>
   );
 }
 
